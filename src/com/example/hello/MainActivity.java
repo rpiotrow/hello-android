d5fbaa2d81a8fdb92bbeefda42c01aaaa3532514
Rafał Piotrowski <rafal.piotrowski@unity.pl>
2013-08-28 12:23:53 +0200

    fragmenty na back stacku

--- src/com/example/hello/MainActivity.java
+++ src/com/example/hello/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.hello;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends FragmentActivity {
+	
+	private int counter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        findViewById(R.id.add_fragment_button).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				buttonClick();
+			}
+		});
+        
       
         if (savedInstanceState == null) {
 	        getSupportFragmentManager()
@@ -19,5 +31,13 @@ public class MainActivity extends FragmentActivity {
         }
         	
     }
+
+	private void buttonClick() {
+		getSupportFragmentManager()
+			.beginTransaction()
+			.replace(R.id.fragment_container, TextFragment.create("New Fragment " + counter++))
+			.addToBackStack(null)
+			.commit();
+	}
 
 }
